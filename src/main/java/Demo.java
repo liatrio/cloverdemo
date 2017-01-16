39bce2017081d2f1652cd86a564c53f1825dd702
Dmitry <dmitry@liatrio.com>
2017-01-16 11:41:54 -0800

    Added more lines

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -5,10 +5,14 @@ public class Demo {
         this.s = "Hello world!";
     }
 
-    public void another_method() {this.s = "it"; }
+    public void another_method() {
+        this.s = "it";
+    }
 
-    public String foo_bar() {
-        if (1==1) return ("Foo");
-//        else return ("Bar");
+    public void foo_bar() {
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
     }
 }
