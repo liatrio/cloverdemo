a00f437cf731b32d076452881d9378e0dc3a0740
Dmitry <dmitry@liatrio.com>
2017-01-17 12:01:11 -0800

    Added a test

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -6,15 +6,19 @@ public class DemoTest {
     private Demo testClass = new Demo();
 
     @Test
-    public void test0() {assertTrue(testClass.another_method().equals("if"));}
+    public void test1() {assertTrue(testClass.another_method().equals("if"));}
 
     @Test
-    public void test1() {
+    public void test2() {
         assertTrue(testClass.s.equals("Hello world!"));
     }
 
     @Test
-    public void test2() {
+    public void test3() {
         assertTrue(testClass.return_string(3,5).equals("b"));
+    }
+
+    @Test
+    public void test4() {assertTrue(testClass.return_string(5,3).equals("b"));
     }
 }
